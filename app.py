4370842f681c2da9c25c2b738e6cf0cd38346a9e
Rupesh Yadav <21rupeshyadav@gmail.com>
2026-05-11 13:03:38 +0530

    ok

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 def add(x,y):
     return x+y
 
-x = int(input("Enter x\n"))
-y = int(input("Enter y\n"))
-print(f"Sum is {add(x,y)}") 
 
 def multiplication(x,y):
     return x*y
+
+x = int(input("Enter x\n"))
+y = int(input("Enter y\n"))
+print(f"Sum is {add(x,y)}")
+print(f"Product is {multiplication(x,y)}")
